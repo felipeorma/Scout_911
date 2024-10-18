18798467d41219831ba0a31fc39bd926dbbdb9cd
CarlosCO94 <164273378+CarlosCO94@users.noreply.github.com>
2024-10-18 09:10:44 -0500

    Add files via upload

---
+++ Pages/Busqueda.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def app():
+    st.header("Página de Búsqueda")
+    st.write("Aquí puedes buscar jugadores y datos específicos.")
+    # Código adicional para la búsqueda
+
+if __name__ == "__main__":
+    app()

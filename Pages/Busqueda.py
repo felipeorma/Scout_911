e5bed712bdd1efcabb1a633726d003a3c5ea8a56
CarlosCO94 <164273378+CarlosCO94@users.noreply.github.com>
2024-10-21 07:56:21 -0500

    Delete Pages directory

--- Pages/Busqueda.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def app():
-    st.header("Página de Búsqueda")
-    st.write("Aquí puedes buscar jugadores y datos específicos.")
-    # Código adicional para la búsqueda
-
-if __name__ == "__main__":
-    app()

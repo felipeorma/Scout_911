c89ae789e008dbaa002b4b140ceeabdbdd776cb4
CarlosCO94 <164273378+CarlosCO94@users.noreply.github.com>
2024-10-30 12:53:21 -0500

    Update 4_puntaje.py

--- pages/4_puntaje.py
+++ pages/4_puntaje.py
@@ -1,45 +1,43 @@
 import streamlit as st
 import pandas as pd
+import requests
+from io import BytesIO
+
+# Configurar la página en formato "wide"
+st.set_page_config(page_title="911_Scout - Scoring", page_icon="⚽", layout="wide")
+
+# Título principal
+st.header("Scoring de Jugadores")
 
 # URL base de los archivos en GitHub
 GITHUB_RAW_BASE = "https://raw.githubusercontent.com/CarlosCO94/Scout_911/main/data/"
 
-# Nombres de los archivos que deseas cargar
-league_files = [
-    "Argentina Copa de la Liga 2024.parquet",
-    "Argentina Primera Nacional 2024.parquet",
-    "Bolivian LFPB 2024.parquet",
-    "Brasileirão 2024.parquet",
-    "Brazil Serie B 2024.parquet",
-    "Brazil Serie C 2024.parquet",
-    "Chilean Primera B 2024.parquet",
-    "Chilean Primera División 2024.parquet",
-    "Colombian Primera A 2024.parquet",
-    "Colombian Torneo BetPlay 2024.parquet",
-    "Ecuador Liga Pro 2024.parquet",
-    "MLS 2024.parquet",
-    "Panama LPF 2024.parquet",
-    "Paraguay Division Profesional 2024.parquet",
-    "Peruvian Liga 1 2024.parquet",
-    "Uruguay Primera División 2024.parquet"
-]
+# Nombres de los archivos de liga
+league_files = {
+    "Argentina Copa de la Liga 2024": "Argentina Copa de la Liga 2024.parquet",
+    "Argentina Primera Nacional 2024": "Argentina Primera Nacional 2024.parquet",
+    "Bolivian LFPB 2024": "Bolivian LFPB 2024.parquet",
+    "Brasileirão 2024": "Brasileirão 2024.parquet",
+    "Brazil Serie B 2024": "Brazil Serie B 2024.parquet",
+    "Brazil Serie C 2024": "Brazil Serie C 2024.parquet",
+    "Chilean Primera B 2024": "Chilean Primera B 2024.parquet",
+    "Chilean Primera Division 2024": "Chilean Primera Division 2024.parquet",
+    "Colombian Primera A 2024": "Colombian Primera A 2024.parquet",
+    "Colombian Torneo BetPlay 2024": "Colombian Torneo BetPlay 2024.parquet",
+    "Ecuador Liga Pro 2024": "Ecuador Liga Pro 2024.parquet",
+    "MLS 2024": "MLS 2024.parquet",
+    "Panama LPF 2024": "Panama LPF 2024.parquet",
+    "Paraguay Division Profesional 2024": "Paraguay Division Profesional 2024.parquet",
+    "Peruvian Liga 1 2024": "Peruvian Liga 1 2024.parquet",
+    "Uruguay Primera División 2024": "Uruguay Primera División 2024.parquet"
+}
 
-# Función para cargar todos los datos en session_state
-def load_all_league_data():
-    if 'data' not in st.session_state:
-        st.session_state['data'] = {}
-        for league_file in league_files:
-            url = GITHUB_RAW_BASE + league_file
-            st.session_state['data'][league_file] = pd.read_parquet(url)
-
-# Función para obtener los datos
-def get_data():
-    if 'data' in st.session_state:
-        return st.session_state['data']
-    return None
-
-# Llamada para cargar todos los datos una vez
-load_all_league_data()
+# Función para cargar datos de una liga específica desde GitHub
+def load_league_data(league_file):
+    url = GITHUB_RAW_BASE + league_file.replace(" ", "%20")
+    response = requests.get(url)
+    response.raise_for_status()  # Asegura que la solicitud se realizó correctamente
+    return pd.read_parquet(BytesIO(response.content))
 
 # Ponderaciones para cada posición
 weights = {
@@ -70,7 +68,7 @@ weights = {
         'Progressive runs per 90': 0.1,
         'Key passes per 90': 0.1
     },
-    'Lateral': {
+    'Lateral Izquierdo': {
         'Successful attacking actions per 90': 0.15,
         'Successful defensive actions per 90': 0.1,
         'Crosses to goalie box per 90': 0.1,
@@ -85,21 +83,58 @@ weights = {
         'Accelerations per 90': 0.05,
         'Progressive runs per 90': 0.1
     },
-    'Mediocampista': {
+    'Lateral Derecho': {
+        'Successful attacking actions per 90': 0.15,
+        'Successful defensive actions per 90': 0.1,
+        'Crosses to goalie box per 90': 0.1,
+        'Aerial duels won, %': 0.1,
+        'Offensive duels won, %': 0.1,
+        'Defensive duels won, %': 0.1,
+        'Duels won, %': 0.05,
+        'Interceptions per 90': 0.05,
+        'Accurate passes to penalty area, %': 0.05,
+        'Third assists per 90': 0.05,
+        'xA per 90': 0.05,
+        'Accelerations per 90': 0.05,
+        'Progressive runs per 90': 0.1
+    },
+    'Mediocampista Defensivo': {
+        'Defensive duels won, %': 0.2,
+        'Interceptions per 90': 0.15,
+        'Aerial duels won, %': 0.1,
+        'Accurate short / medium passes, %': 0.1,
+        'Accurate long passes, %': 0.1,
+        'Offensive duels won, %': 0.05,
+        'xA per 90': 0.05,
+        'Progressive runs per 90': 0.05,
+        'Successful dribbles, %': 0.05,
+        'Goals per 90': 0.05,
+        'Received passes per 90': 0.05,
+        'Accurate progressive passes, %': 0.05
+    },
+    'Mediocampista Central': {
         'Assists per 90': 0.15,
         'xA per 90': 0.1,
-        'Offensive duels won, %': 0.1,
-        'Aerial duels won, %': 0.05,
-        'Defensive duels won, %': 0.05,
+        'Accurate passes to final third, %': 0.15,
+        'Defensive duels won, %': 0.1,
         'Interceptions per 90': 0.1,
-        'Received passes per 90': 0.05,
+        'Received passes per 90': 0.1,
         'Accurate short / medium passes, %': 0.1,
-        'Accurate passes to final third, %': 0.1,
         'Accurate long passes, %': 0.05,
-        'Accurate progressive passes, %': 0.05,
+        'Progressive runs per 90': 0.1,
+        'Goals per 90': 0.05
+    },
+    'Mediocampista Ofensivo': {
+        'Assists per 90': 0.2,
+        'xA per 90': 0.15,
+        'Key passes per 90': 0.15,
+        'Accurate passes to penalty area, %': 0.1,
         'Successful dribbles, %': 0.1,
-        'xG per 90': 0.05,
-        'Goals per 90': 0.1
+        'Goals per 90': 0.1,
+        'Offensive duels won, %': 0.1,
+        'Accurate forward passes, %': 0.05,
+        'Accurate progressive passes, %': 0.05,
+        'Interceptions per 90': 0.05
     },
     'Extremos': {
         'xG per 90': 0.15,
@@ -134,62 +169,67 @@ weights = {
         'Duels won, %': 0.05,
         'Shots per 90': 0.05,
         'Shots on target, %': 0.05,
-        'Touches in box per 90': 0.05}
+        'Touches in box per 90': 0.05
+    }
+    # Agrega ponderaciones para otras posiciones aquí...
 }
 
-# Función para calcular el puntaje basado en la posición
+# Función para calcular el puntaje basado en la posición, validando métricas
 def calculate_score(row, position):
     score = 0
     if position in weights:
         for metric, weight in weights[position].items():
-            score += row.get(metric, 0) * weight
+            if metric in row and pd.notna(row[metric]):  # Validación de existencia y no nulo
+                score += row[metric] * weight
     return score
 
-# Configuración de la página de Streamlit
-st.title("Sistema de Scoring de Jugadores por Liga")
-st.write("Selecciona una liga y una posición para calcular el scoring de los jugadores.")
-
 # Selección de liga
-selected_league = st.selectbox("Selecciona la liga", league_files)
-data = get_data()[selected_league] if get_data() else None
+selected_league = st.selectbox("Selecciona la liga para cargar datos", list(league_files.keys()))
 
-# Selección de posición
-selected_position = st.selectbox("Selecciona la posición", [
-    'Arquero', 'Defensa', 'Lateral Izquierdo', 'Lateral Derecho', 
-    'Mediocampista Defensivo', 'Mediocampista Central', 'Mediocampista Ofensivo', 
-    'Extremos', 'Delantero'
-])
+# Cargar los datos del archivo seleccionado
+if selected_league:
+    try:
+        data = load_league_data(league_files[selected_league])
+        st.write(f"Datos cargados para: {selected_league}")
 
-if data is not None:
-    # Aplicar filtro de posición
-    if selected_position == 'Arquero':
-        df = data[data['Position'].str.contains('GK', na=False)]
-    elif selected_position == 'Defensa':
-        df = data[data['Position'].str.contains('CB', na=False)]
-    elif selected_position == 'Lateral Izquierdo':
-        df = data[data['Position'].str.contains('LB|LWB', na=False)]
-    elif selected_position == 'Lateral Derecho':
-        df = data[data['Position'].str.contains('RB|RWB', na=False)]
-    elif selected_position == 'Mediocampista Defensivo':
-        df = data[data['Position'].str.contains('DMF', na=False)]
-    elif selected_position == 'Mediocampista Central':
-        df = data[data['Position'].str.contains('CMF', na=False)]
-    elif selected_position == 'Mediocampista Ofensivo':
-        df = data[data['Position'].str.contains('AMF', na=False)]
-    elif selected_position == 'Extremos':
-        df = data[data['Position'].str.contains('RW|LW|LWF|RWF', na=False)]
-    elif selected_position == 'Delantero':
-        df = data[data['Position'].str.contains('CF', na=False)]
-    
-    # Calcular el scoring para la posición filtrada
-    df['Score Total'] = df.apply(lambda row: calculate_score(row, selected_position), axis=1)
+        # Selección de posición
+        selected_position = st.selectbox("Selecciona la posición para calcular scoring", [
+            'Arquero', 'Defensa', 'Lateral Izquierdo', 'Lateral Derecho', 
+            'Mediocampista Defensivo', 'Mediocampista Central', 'Mediocampista Ofensivo', 
+            'Extremos', 'Delantero'
+        ])
 
-    # Normalizar los puntajes a una escala de 1 a 10
-    min_score, max_score = df['Score Total'].min(), df['Score Total'].max()
-    df['Score Total'] = df['Score Total'].apply(lambda x: 1 + (x - min_score) * 9 / (max_score - min_score) if max_score > min_score else 1)
+        # Filtrar los datos según la posición seleccionada usando 'Primary position'
+        position_map = {
+            'Arquero': 'GK',
+            'Defensa': 'CB',
+            'Lateral Izquierdo': 'LB|LWB',
+            'Lateral Derecho': 'RB|RWB',
+            'Mediocampista Defensivo': 'DMF',
+            'Mediocampista Central': 'CMF',
+            'Mediocampista Ofensivo': 'AMF',
+            'Extremos': 'RW|LW|LWF|RWF',
+            'Delantero': 'CF'
+        }
+        
+        if selected_position in position_map:
+            df = data[data['Primary position'].str.contains(position_map[selected_position], na=False)]
 
-    # Seleccionar las columnas deseadas y mostrar solo los 10 primeros resultados
-    st.write(f"Scoring calculado para la posición: {selected_position}")
-    st.dataframe(df[['Full name', 'Team within selected timeframe', 'Age', 'Passport country', 'Score Total']].head(10))
+        # Calcular el scoring para la posición filtrada
+        df['Score Total'] = df.apply(lambda row: calculate_score(row, selected_position), axis=1)
+
+        # Normalizar los puntajes a una escala de 1 a 10
+        min_score, max_score = df['Score Total'].min(), df['Score Total'].max()
+        if min_score != max_score:
+            df['Score Total'] = df['Score Total'].apply(lambda x: 1 + (x - min_score) * 9 / (max_score - min_score))
+        else:
+            df['Score Total'] = 5  # Puntaje central si no hay variación en los valores
+
+        # Mostrar los resultados
+        st.write(f"Scoring calculado para la posición: {selected_position} en {selected_league}")
+        st.dataframe(df[['Full name', 'Team within selected timeframe', 'Age', 'Passport country', 'Score Total']].head(10))
+
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error al cargar los datos: {e}")
 else:
-    st.write("No se encontraron datos en session_state. Por favor, carga los datos antes de continuar.")
+    st.write("Selecciona una liga para comenzar.")

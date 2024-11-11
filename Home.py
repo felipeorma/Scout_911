dbe72cd73afe8cd228bb5934984659f9e65d0d1e
CarlosCO94 <164273378+CarlosCO94@users.noreply.github.com>
2024-11-10 20:27:37 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="911_Scout", page_icon="⚽", layout="wide")
 
 # Título principal y subtítulo
 st.header("Carga de datos")
-st.subheader("Desarrollado por: Carlos Eduardo Canal Ortiz")
+st.subheader("By: CECO")
 
 # URL base de los archivos en GitHub
 GITHUB_RAW_BASE = "https://raw.githubusercontent.com/CarlosCO94/Scout_911/main/data/"
@@ -18,20 +18,36 @@ league_files = [
     "Argentina Copa de la Liga 2024.parquet",
     "Argentina Primera Nacional 2024.parquet",
     "Bolivian LFPB 2024.parquet",
-    "Brasileirão 2024.parquet",
+    "Brasileirao 2024.parquet",
     "Brazil Serie B 2024.parquet",
     "Brazil Serie C 2024.parquet",
+    "Canadian Premier League 2024.parquet",
     "Chilean Primera B 2024.parquet",
-    "Chilean Primera División 2024.parquet",
+    "Chilean Primera Division 2023.parquet",
+    "Chilean Primera Division 2024.parquet",
     "Colombian Primera A 2024.parquet",
     "Colombian Torneo BetPlay 2024.parquet",
     "Ecuador Liga Pro 2024.parquet",
     "MLS 2024.parquet",
+    "MLS Next Pro 2024.parquet",
     "Panama LPF 2024.parquet",
     "Paraguay Division Profesional 2024.parquet",
     "Peruvian Liga 1 2024.parquet",
-    "Uruguay Primera División 2024.parquet"
+    "USL Championship 2024.parquet",
+    "Uruguay Primera Division 2024.parquet",
 ]
+
+# Estilo CSS para ajustar el ancho de la tabla y centrar los valores
+st.markdown("""
+    <style>
+    .dataframe {
+        width: 100% !important;
+    }
+    .stDataFrame tbody tr td {
+        text-align: center !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
 # Función para cargar los datos desde GitHub
 @st.cache_data
@@ -58,14 +74,16 @@ if st.button("Cargar Datos") or 'data' in st.session_state:
 
     # Mostrar el contenido de la data si existe
     st.write("Archivos cargados:")
-    # Seleccionar solo las columnas que quieres mostrar
     columnas_a_mostrar = ['Full name', 'Team within selected timeframe', 'Age', 'Position', 'Passport country']
     
     # Verificar que las columnas existan en el DataFrame
     if all(col in st.session_state['data'].columns for col in columnas_a_mostrar):
-        st.dataframe(st.session_state['data'][columnas_a_mostrar].head(10))  # Muestra solo los primeros 10 registros
+        df_filtered = st.session_state['data'][columnas_a_mostrar].head(10)  # Muestra solo los primeros 10 registros
+        
+        # Mostrar la tabla sin formato condicional
+        st.write("Vista previa de los datos:")
+        st.dataframe(df_filtered)
     else:
         st.warning("No se encontraron todas las columnas necesarias en los datos.")
 else:
     st.info("Haz clic en el botón 'Cargar Datos' para cargar los archivos.")
-
